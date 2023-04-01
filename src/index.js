6ff51b049198e1e1dfec4f8691a626ec83ddd7eb
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-04-01 23:43:01 +0300

    add import to js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-import './css/styles.css';
-
+import './sass/index.scss';
+import NewApiService from './js/api-service';
+import { lightbox } from './js/lightbox';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
