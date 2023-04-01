cd72d7af58f5062dff6828b3933c2a04ac5e463f
TetianaUstimevko <107365766+TetianaUstimevko@users.noreply.github.com>
2023-04-01 23:45:52 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './sass/index.scss';
-import NewApiService from './js/api-service';
-import { lightbox } from './js/lightbox';
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+// import './sass/index.scss';
+// import NewApiService from './js/api-service';
+// import { lightbox } from './js/lightbox';
+// import { Notify } from 'notiflix/build/notiflix-notify-aio';
